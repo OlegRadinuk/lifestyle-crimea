8fd22e0723fe7e743f4b2a9f13877b79e937d04f
Oleg <radinuko@gmail.com>
2026-03-09 22:40:53 +0300

    prof_adaptive30

--- data/panoramas.ts
+++ data/panoramas.ts
@@ -57,7 +57,7 @@ export const PANORAMAS: Panorama[] = [
     meta: ['До 4 гостей', '88 м²', 'Панорамные окна', 'Зона отдыха'],
   },
   {
-    id: 'ls-lux-sunny-mood', // Замена для ls-lux-sunshine
+    id: 'ls-lux-sunny-mood', // Это дубль! Должно быть для Sunshine
     title: ' Lux Sunshine',
     image: '/panoramas/LS-Lux-Sunshine.webp',
     maxGuests: 2,
